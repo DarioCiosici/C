8a750b761c37e85be86ae1ba55ed95f69a2e7576
DarioCiosici <94352108+DarioCiosici@users.noreply.github.com>
2022-02-11 10:09:12 +0100

    Add files via upload

---
+++ CIosici.Dario.Compreso.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int N1, N2;
+    printf("==Inserisci due valori, in modo che il secondo sia maggiore del primo==\n");
+    scanf("%d", &N1);
+    scanf("%d", &N2);
+    
+    while(N1<0||N2<0)
+    {
+    	printf("==Valori non validi, inseriscili di nuovo==\n");
+        scanf("%d", &N1);
+        scanf("%d", &N2);
+    }
+	while (N2 < N1)
+    {
+        printf("==Valori non validi, inseriscili di nuovo==\n");
+        scanf("%d", &N1);
+        scanf("%d", &N2);
+    }
+    for (int i = N1; i < N2; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
