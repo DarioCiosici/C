6539df004c8c9aca52a51304d8b50f593b37e0cd
DarioCiosici <94352108+DarioCiosici@users.noreply.github.com>
2022-02-11 10:20:09 +0100

    Update CIosici.Dario.Compreso.c

--- CIosici.Dario.Compreso.c
+++ CIosici.Dario.Compreso.c
@@ -6,18 +6,13 @@ int main()
     scanf("%d", &N1);
     scanf("%d", &N2);
     
-    while(N1<0||N2<0)
+    while(N1<0||N2<0&&N2<N1)
     {
     	printf("==Valori non validi, inseriscili di nuovo==\n");
         scanf("%d", &N1);
         scanf("%d", &N2);
     }
-	while (N2 < N1)
-    {
-        printf("==Valori non validi, inseriscili di nuovo==\n");
-        scanf("%d", &N1);
-        scanf("%d", &N2);
-    }
+	
     for (int i = N1; i < N2; i++)
     {
         printf("%d\n", i);
