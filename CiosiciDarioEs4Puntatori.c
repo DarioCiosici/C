4b6a7150a4d920090eca8bf69d43e9a23a07e8a5
DarioCiosici <94352108+DarioCiosici@users.noreply.github.com>
2022-04-10 21:45:38 +0200

    Add files via upload

--- CiosiciDarioEs4Puntatori.c
+++ CiosiciDarioEs4Puntatori.c
@@ -1,94 +1,129 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limit.h>
-int ricerca(int array[],int x)
+
+#include <limits.h>
+#define DIM SHRT_MAX
+
+int ricerca(int array[], int x)
 {
     int numero;
-    printf("inserisci il numero da ricercare");
-    scanf("%d",numero);
-    for(int i=0;i<x;i++)
+    printf("inserisci il numero da ricercare\n");
+    scanf("%d", &numero);
+    for (int i = 0; i < x; i++)
     {
-        if(array[i]==numero)
-            return i+1;
+        if (array[i] == numero)
+            return i + 1;
     }
     return -1;
 }
-void visualizza(int array[],int x)
+void visualizza(int array[], int *x)
 {
-    for(int i=0;i<x;i++)
+    for (int i = 0; i < *x; i++)
     {
-        printf("%d",array[i]);
+        printf("%d ", array[i]);
     }
     printf("\n");
 }
-void inserisci(int array[],int x)
+void inserisci(int array[], int *x)
 {
     int numero;
-    printf("inserisci il numero che vuoi inserire");
-    scanf("%d",numero);
-    array[x]=numero;
-    printf("Numero inserito in %d",x);
-    x++;
-
+    printf("inserisci il numero che vuoi inserire\n");
+    scanf("%d", &numero);
+    array[*x+1] = numero;
+    printf("Numero inserito in %d\n", *x);
+    
 }
 void modifica(int array[], int x)
 {
-    int numero,posizione;
-    printf("Inserisci l'elemento che vuoi modifcare, disponibile fino a %d",x);
-    scanf("%d",posizione);
-    printf("Inserisci il nuovo numero");
-    scanf("%d", numero);
-    array[posizione]=numero;
+    int numero, posizione;
+    printf("Inserisci l'elemento che vuoi modifcare, disponibile fino a %d\n", x);
+    scanf("%d", &posizione);
+    printf("Inserisci il nuovo numero\n");
+    scanf("%d", &numero);
+    array[posizione] = numero;
 }
-void elimina(array[],int x)
+void elimina(int array[], int *x)
 {
     int posizione;
-    printf("Inserisci l'elemento che vuoi eliminare, disponibile fino a %d",x);
-    scanf("%d",posizione);
-    for(int i=posizione;i<x-1;i++)
+    printf("Inserisci l'elemento che vuoi eliminare, disponibile fino a %d\n", *x);
+    scanf("%d", &posizione);
+    for (int i = posizione; i < *x - 1; i++)
     {
-        array[i]=array[i+1];
+        array[i] = array[i + 1];
     }
-    array[x]=0;
+    array[*x] = 0;
 }
-int main(int argc,char *argv[])
+void visualizzaInv(int array[], int x)
 {
-    int numeri[INT_MAX];
-    int scelta,x=0,risp;
+    for (int i = *x; i >=0; i--)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+void inserisciPos(int array[]int x)     //difficolta nel mandare indietro l'array
+{
+    int pos,numero;
+    printf("Inserisci il numero da inserire\n");
+    scanf("%d", numero);
+    printf("Inserisci dove vuoi inserirlo\n");
+    scanf("%d",pos);
+    int tmp=array[pos+1];
+    for(int i=pos;i<x;i++)
+    {
+        int tmp1;
+      
+        array[]
+
+
+    }
+}
+int main(int argc, char *argv[])
+{
+    int numeri[DIM];
+    int scelta, x = 0, risp;
     do
     {
-    printf("Inserisci una serie di numeri, mettere -1 per procedere");
-    scanf("%d",scelta);
-    if(scelta!=-1)
-    {
-        numeri[x]=scelta;
-        x++;
-    }
-    }while(scelta!=-1);
-    
+        printf("Inserisci una serie di numeri, mettere -1 per procedere\n");
+        scanf("%d", &scelta);
+        if (scelta > 0)
+        {
+            numeri[x] = scelta;
+            x++;
+        }
+    } while (scelta > -1);
+
     do
     {
         printf("premere 1 per mostrare il contenuto dell'array\n");
-	    printf("premere 2 per inserire un elemento\n");
-	    printf("premere 3 per modificare un elemento\n");
-	    printf("premere 4 per eliminare un elemento\n");
-	    printf("premere 5 per ricercare un elemento\n");
-	    printf("premere 0 per terminare"\n);
-        scanf("%d",scelta);
-        if(scelta==1)
-            ricerca(numeri[],x);
-        else if(scelta==2)
-            inserisci(numeri[],x);
-        else if(scelta==3)
-            modifica(numeri[],x);
-        else if(scelta==4)
-            elimina(numeri[],x);
-        else if(scelta==5)
+        printf("premere 2 per inserire un elemento\n");
+        printf("premere 3 per modificare un elemento\n");
+        printf("premere 4 per eliminare un elemento\n");
+        printf("premere 5 per ricercare un elemento\n");
+        printf("premere 6 per visualizzare l'array al contrario\n");
+        printf("premere 7 per inserire un numero in una posizione");
+        printf("premere 0 per terminare\n");
+        scanf("%d", &scelta);
+        if (scelta == 1)
+            visualizza(numeri, &x);
+        else if (scelta == 2)
         {
-            risp=ricerca(numeri[],x);
-            if(risp!=-1)
-                printf("In numero si trova in posizione %d", risp);
+            inserisci(numeri, &x);
+            x++;
         }
-    }while(scelta!=0);
+        else if (scelta == 3)
+            modifica(numeri, x);
+        else if (scelta == 4)
+            elimina(numeri, &x);
+        else if (scelta == 5)
+        {
+            risp = ricerca(numeri, x);
+            if (risp != -1)
+                printf("In numero si trova in posizione %d\n", risp);
+        }
+        else if(scelta==6)
+            visualizzaInv(numeri, &x);
+        else if(scelta==7)
+            inserisciPos(numeri);
+    } while (scelta > 0);
     return 0;
 }
